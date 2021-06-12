bc711862961c2fcc18904e229899a154fbc30776
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-06-11 20:47:07 -0500

    Subiendo cambios para que funcione el server.

---
+++ config/env/production/database.js
@@ -0,0 +1,27 @@
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  const config = parse(process.env.DATABASE_URL)
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejecjtUnauthorized: false,
+          }
+        },
+        options: {
+          ssl: false,
+        },
+      },
+    },
+  };
+};
